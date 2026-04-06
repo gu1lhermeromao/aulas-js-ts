4b1ebdc58c0646fbf4799dd5644f3c1040785618
gu1lhermeromao <gu1lhermeromao@protonmail.com>
2026-04-06 15:40:59 -0300

    quarto_commit

--- aula35/assets/js/main.js
+++ aula35/assets/js/main.js
@@ -1,10 +1,27 @@
 const elementos = [
-    {tag: 'p', texto: 'Frase 1'},
-    {tag: 'div', texto: 'Frase 2'},
-    {tag: 'footer', texto: 'Frase 3'},
-    {tag: 'section', texto: 'Frase 4'}
+    { tag: 'p', texto: 'Frase 1' },
+    { tag: 'div', texto: 'Frase 2' },
+    { tag: 'footer', texto: 'Frase 3' },
+    { tag: 'section', texto: 'Frase 4' }
 ]
 
+function criarDiv() {
+    let div = document.createElement('div');
+    let container = document.querySelector('.container');
+    container.appendChild(div);
+    return div;
+}
+
+let minhaDiv = criarDiv()
+
+function criarElemento(tag, texto) {
+    let elemento = document.createElement(tag);
+    elemento.innerHTML = texto;
+    minhaDiv.appendChild(elemento);
+}
+
 for (let i = 0; i < elementos.length; i++) {
-    return elementos[i];
+    let tag = elementos[i].tag;
+    let texto = elementos[i].texto;
+    criarElemento(tag, texto);
 }
