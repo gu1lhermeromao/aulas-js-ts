5a3073401160abe5260c7a97d6e98924b9b7a6d6
gu1lhermeromao <gu1lhermeromao@protonmail.com>
2026-04-06 13:49:16 -0300

    terceiro_commit

---
+++ aula35/assets/js/main.js
@@ -0,0 +1,10 @@
+const elementos = [
+    {tag: 'p', texto: 'Frase 1'},
+    {tag: 'div', texto: 'Frase 2'},
+    {tag: 'footer', texto: 'Frase 3'},
+    {tag: 'section', texto: 'Frase 4'}
+]
+
+for (let i = 0; i < elementos.length; i++) {
+    return elementos[i];
+}
